e9b4fa9af374873b8d14af4bc284776ba45b408d
Saiyan <saiyan@cssai.eu>
2014-09-23 21:42:32 +0200

    added first content to README.md. config.js got comments for better understanding.

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-ownshort
-==============
+#ownshort
 
 This is ownshort.
+
+##Requirements
+[node-sqlite3](https://github.com/mapbox/node-sqlite3)
+> The sqlite3 module works with Node.js v0.10.x or v0.11.x (though only v0.11.13 and above).
+
+##Installation
+
+    git clone https://github.com/Saiyan/ownshort.git
+    cd ownshort
+    npm install sqlite3
+
+##Running the server
+
+    nodejs /path/to/ownshort/ownshort.js
+
+now you can access the frontend under http://localhost:9080
+
+##Options
+Adjust the config.js file to your needs
+
--- config.js
+++ config.js
@@ -1,26 +1,32 @@
 
 var config = {
+    //Settings for the HTTP server
     http:{
         enabled: true,
         port: 9080
     },
+    //Settings for the HTTPS server
     https: {
         enabled: false,
         port: 9081,
         keyfile: "../temp/server.key",
         certfile: "../temp/server.crt"
     },
-    files:{
-        favicon: 'favicon.ico'
-    },
+    //Database settings
     db:{
         filename:"urls.sqlite3"
     },
+    //Frontend settings
     frontend:{
         theme: {
+            //name of the folder for the active theme (e.g. "simple" loads themes/simple/)
             active: 'simple',
+            
+            //If this option is enabled the server will load the theme only on startup.
+            //This could be handy if you are developing your own theme and dont want to restart the server everytime you make a change.
             loadonce: true
         },
+        //If the Frontend is disabled you can only use http://SERVER/?url=LINK and the short links
         disabled: false
     }
 };
